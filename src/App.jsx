853bfd08b2b09000e6b5614160c06c8682f4a161
Aakankshapatil123 <aakankshapatil6580@gmail.com>
2026-04-29 12:30:59 +0530

    likes counter with useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,30 @@
-// updating a normal variablr does not re-rendering of the the component
-//re-rendering means that the component will be re-evaluted and the UI will 
-// be updated with nwe values
+// state: a way store and manage data in a component (componet memory)
+// state is use to store data that can over time and affect the rendering component
+// in react we can useState hook to manage state in functional componets
+// the useState is function that use the array
+// the array have a stateful value and function to update it.
+
+// Note: we should no update or mutate the state directly
+// Ṇote: State change cause re-rendering
+
+import { useState } from "react"
+
 
 const App = () => {
-   let likes = 0;
 
-   const handleLike = () => {
-    // update the likes variable
-     likes++; // or likes = likes + 1;
-     console.log(likes); // this will log the updated value of likes
-   };
+  const[likes, setLike] = useState(0)
 
-  //  when like button is clicked, it will call handlelike function
+  const likeHandle = () => {
+     setLike(likes + 1);
+  }
+
+  // when the like button is clicked, it will call likehandle function
   return (
     <>
-    <h1>Like : {likes}</h1>
-    <button onClick={handleLike}>Like</button>
+    <h1>Likes : {likes}</h1>
+    <button onClick={likeHandle}>Like</button>
     </>
   )
 }
 
-export default App;
+export default App
