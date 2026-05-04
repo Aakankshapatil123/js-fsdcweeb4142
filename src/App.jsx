7c178c71c68716bf915168de93df543e8e396551
Aakankshapatil123 <aakankshapatil6580@gmail.com>
2026-05-04 12:06:03 +0530

    list rendering without map method

--- src/App.jsx
+++ src/App.jsx
@@ -1,70 +1,31 @@
-// controlled component or controlled form
 
-import { useState } from "react";
 
 const App = () => {
-  // 1. create a state for the input
-   const[email, steEmail] = useState("");
-   const[password, setPassword] = useState("");
+  const recipes = [
+    'Biryani',
+    'Pulao',
+    'Fried Rice',
+    'Noodles',
+    'Pasta',
+    'Pizza',
+    'Burger',
+    'Sandwich',
+    'Salad',
+    'Soup'
+  ];
 
-  const handleLogin = (event) => {
-    event.preventDefault();
-    console.log("Logging in....");
-    console.log(email)
-    console.log(password);
+  // const list = [<li>recipes</li>] 
 
-
-    // reset the form
-    steEmail("");
-    setPassword("");
-
-    event.target.email.focus();
-  }
-
-  
+  const list = [];
+  recipes.forEach(recipes => {
+    list.push(
+    <li>{recipes}</li>
+  )
+  });
   return (
     <>
-    
-    <form onSubmit={handleLogin}
-    className="flex flex-col gap-4 w-100 mx-auto mt-10 bprder 
-    border-gray-300 rounded p-8 shadow-lg"
-    >
-     <h1
-      className="text-2xl font-bold text-center"
-     >Login</h1>
-     <div
-      className="flex flex-col gap-2"
-     >
-       <input
-       name="email"
-      type='email'
-      placeholder='email...'
-      value={email}
-      onChange={(e) => steEmail(e.target.value)}
-      required
-      className="border border-gray-300 rounded px-3 py-2"
-      />
-     </div>
-
-     <div>
-      <input
-      name="password"
-      type='password'
-      placeholder='password...'
-      value={password}
-      onChange={(e) => setPassword(e.target.value)}
-      required
-      className="border border-gray-300 rounded px-3 py-2 w-full"
-      />
-     </div>
-
-     <button 
-     type="submit"
-     className="bg-yellow-500 rounded px-3 py-2 w-full"
-     >
-      Login
-     </button>
-    </form>
+    <h1>Recipes</h1>
+    <ul>{list}</ul>
     </>
   )
 }
