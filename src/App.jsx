03343b0250b9f401a82574a19921435a2adf48ed
Aakankshapatil123 <aakankshapatil6580@gmail.com>
2026-05-07 16:06:12 +0530

    state counter --useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,16 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios';
-import Todo from './components/Todo';
+import { useState } from "react";
+
 
 const App = () => {
 
-  const [todos, setTodos] = useState([])
+  const [likes, setLikes] = useState(0)
 
-  // 1.with dependancy list
- useEffect(() => {
-  // run when the component is rendered or re-renderd when the specified dependencies.
-   axios.get('https://69fc3366fce564e259176e6d.mockapi.io/todos')
-  .then(reponse => setTodos(reponse.data));
- },[]);
-
-
-//  console.log(count1);
   return (
     <>
-    <h1>Todes</h1>
-     <ul>
-      {
-        todos
-        .map(todo =>{
-          return <Todo 
-          key={todo.id}
-          todo={todo}
-          />
-        })
-      }
-     </ul>
+    <h1>Like: {likes}</h1>
+    <button onClick={() => setLikes(likes + 1)}>Like</button>
     </>
   )
 }
 
-export default App
+export default App;
