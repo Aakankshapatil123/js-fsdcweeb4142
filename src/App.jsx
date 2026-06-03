1ef4881312ee01f746daaaa499f80d94ab677b4d
Aakankshapatil123 <aakankshapatil6580@gmail.com>
2026-06-03 10:51:18 +0530

    state manegment: useReducer hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
-import { useState } from "react";
+import { useReducer, useState } from "react";
+
+const reducer = (state, action) => {
+  // the current state is in -> state
+  // has to return the new state
+  if(action.type === "LIKE"){
+    // we know that the like buttoon is clicked
+    // so we want to increace the current state by 1 retun it
+    return state + 1
+  }
+
+}
 
 const App = () => {
-  const [likes, setLikes] = useState(0);
+  const [likes, setLikes] = useReducer(reducer, 0);
 
   return (
     <div>
       <h1>Likes: {likes}</h1>
-      <button onClick={() => setLikes(likes + 1)}>Like</button>
+      <button onClick={() => setLikes({
+        type: "LIKE"
+      })}>Like</button>
     </div>
   )
 }
