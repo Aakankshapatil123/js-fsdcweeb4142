5da034e6132e5c2eb6d87d4e8746cd572dd63f9c
Aakankshapatil123 <aakankshapatil6580@gmail.com>
2026-06-15 16:51:56 +0530

    emotion-aware system

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 // remove the comment line below to enable tailwind css
-// import './index.css'
+import './index.css'
 
 createRoot(document.getElementById('root')).render(
- <div>
-   <App />
- </div>
+ 
+   <div>
+     <App />
+   </div>
+ 
 )
